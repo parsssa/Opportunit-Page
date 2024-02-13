1c1005215bd86889ed43dd7edfe0b64e1b46108c
Parsa Bagheri <parsssa13@gmail.com>
2024-02-13 18:40:52 +0100

    piccoli fix css

--- oportunit/src/Components/NavigationBar/LoginButton.jsx
+++ oportunit/src/Components/NavigationBar/LoginButton.jsx
@@ -1,13 +1,14 @@
 // LoginButton.jsx
 
 import React from 'react';
+import NavbarLink from './NavbarLink';
 import IconProfile from '../../images/icon-profile.svg'; // Assicurati di impostare il percorso corretto
 
 const LoginButton = () => {
   return (
     <div className="login-button">
       <img src={IconProfile} alt="Io Image" />
-      <button>Accedi</button>
+      <button><NavbarLink to="/link2" className="navbar-link">Accedi</NavbarLink></button>
     </div>
   );
 };
