7febf97c0f39ecd34c53fe4e4386c23fe8d46c30
Parsa Bagheri <parsssa13@gmail.com>
2024-02-23 11:03:47 +0100

    tolto autocandidatura

--- oportunit/src/Components/Search/Filter.jsx
+++ oportunit/src/Components/Search/Filter.jsx
@@ -14,35 +14,40 @@ const Filter = ({ label, options, selectedOption = [], onFilterChange }) => {
   }, [options, selectedOption, onFilterChange]);
 
   const handleCheckboxChange = (value) => {
+    const updatedOptions = selectedOption ? [...selectedOption] : [];
+  
     if (value === 'Tutte') {
       // If "Tutte" is checked, select all options
-      const updatedOptions = selectedOption && selectedOption.length > 0 && selectedOption.includes('Tutte')
-        ? [] // Uncheck "Tutte" if it was already checked
-        : options.map(option => option.value);
-      onFilterChange(updatedOptions);
+      const allOptionsChecked = updatedOptions.length === options.length;
+  
+      if (allOptionsChecked) {
+        // Uncheck all options if everything is checked
+        onFilterChange([]);
+      } else {
+        // Check all options
+        const allOptions = options.map(option => option.value);
+        onFilterChange(allOptions);
+      }
     } else {
-      // Toggle the option
-      const updatedOptions = selectedOption ? [...selectedOption] : [];
       const index = updatedOptions.indexOf(value);
-
+  
       if (index === -1) {
         updatedOptions.push(value);
       } else {
         updatedOptions.splice(index, 1);
       }
-
-      // If "Tutte" is unchecked and any individual option is unchecked, remove it from the selection
-      if (!updatedOptions.includes('Tutte') && updatedOptions.length > 0) {
-        const indexOfTutte = updatedOptions.indexOf('Tutte');
-        if (indexOfTutte !== -1) {
-          updatedOptions.splice(indexOfTutte, 1);
-        }
+  
+      // If "Tutte" is unchecked, uncheck all other options
+      const tutteIndex = updatedOptions.indexOf('Tutte');
+      if (tutteIndex !== -1) {
+        updatedOptions.splice(tutteIndex, 1);
       }
-
+  
       onFilterChange(updatedOptions);
     }
   };
-
+  
+  
   return (
     <div className="filter">
       <button className="filter-button" onClick={() => setIsOpen(!isOpen)}>
